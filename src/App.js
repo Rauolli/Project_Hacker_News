e810189cecb40dc972f568146912ccc970875bc5
Rauolli <51509754+Rauolli@users.noreply.github.com>
2022-08-25 20:25:05 +0200

    added Styles and a function to show the correct timespan since the creationtime an today

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import Header from "./Components/Header";
+import "./App.css";
 
 export default function App() {
   const [news, setNews] = useState([]);
   const [searchWord, setSearchWord] = useState([]);
+  
 
   const URI = `https://hn.algolia.com/api/v1/search?query=${searchWord}`;
 
@@ -14,14 +16,15 @@ export default function App() {
       .catch((err) => console.log(err));
   }, []);
 
-  console.log(news);
+  console.log("News:" + news);
 
-  console.log(searchWord);
+  //console.log("Search Word" + searchWord);
 
-  function checkUserInput(e) {
-	e.preventDefault();
-	setSearchWord(document.getElementById("input").value);
-	}
+  	function checkUserInput(e){
+		e.preventDefault();
+		setSearchWord(document.getElementById("input").value);
+  	}
+
 	function shortUrl (url) {
 		if (url) {
 			let parts = url.split("//");
@@ -31,36 +34,80 @@ export default function App() {
 		}
 	}
 
-	function showAllMessages(n){
-		//console.log(n);
-		let news = [];
+	// function showAllMessages(n){
+	// 	//console.log(n);
+	// 	let news = [];
 	
-		for (let item in n) {
-			news.push(` ${n[item]}`); // ${item}
+	// 	for (let item in n) {
+	// 		news.push(` ${n[item]}`); // ${item}
+	// 	}
+	// 	return news;
+	// }
+
+	function calculateDateDiff(createdDate){
+		const d = new Date(createdDate);
+		const year = d.getFullYear();
+		const month = d.getMonth();
+		const day = d.getDate();
+		const hour = d.getHours();
+		const monthDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+		console.log("created at:" + createdDate);
+		const datum = new Date();
+		const heuteY = datum.getFullYear();
+		const heuteM = datum.getMonth();
+		const heuteD = datum.getDate();
+		const heuteH = datum.getHours();
+
+		let yearDiff = heuteY - year;
+		let monthDiff = 0;
+		if(heuteM > month){
+			monthDiff = heuteM - month;	
+		}else if(heuteM < month){
+			yearDiff -= 1;
+			monthDiff = (12 + heuteM) - month;
 		}
-		return news;
+		
+		let dayDiff = 0; 
+		if(heuteD > day){
+			dayDiff = heuteD - day;
+		}else if(heuteD < day){
+			monthDiff -= 1;
+			dayDiff = (monthDays[heuteD] + heuteD) - day;
+		}
+
+		let hourDiff = 0;
+		if(heuteH > hour){
+			hourDiff = heuteH - hour;
+		}else if(heuteH < hour){
+			dayDiff -= 1;
+			hourDiff = (24 + heuteH) - hour;
+		}
+		return `${yearDiff} years, ${monthDiff} months, ${dayDiff} days, ${hourDiff} hours ago`;
 	}
 
-  return (
-    <div className="App">
-		<div className="flex-row">	
-      		<Header />
-			<input name="input" id="input" type="text" />
-            <button onClick={checkUserInput}>Search</button>
-	  	</div>
+    //const messages = news.map((nachricht) => showAllMessages(nachricht));
+	//console.log("Messages: " + messages);
+
+  	return (
+    	<div className="App">
+			<div className="flex-row">	
+      			<Header />
+				<input name="input" id="input" type="text" />
+            	<button onClick={checkUserInput}>Search</button>
+	  		</div>
 			<hr/>
 			<ol className="table-row">
-      {news.map((nachricht) => ( 
+      			{news.map((nachricht) => ( 
         
-				<>					
-				<p>{nachricht[searchWord]}</p>
-
-				<li><a href={nachricht["url"]}>{nachricht["title"]}</a> (<a href={nachricht["url"]}>{shortUrl(nachricht["url"])}</a>)
-				<br/>{nachricht["points"]} points {nachricht["author"]} {nachricht["created_at"]} {nachricht["num_comments"]}</li>	
-				</>
-			))}
-			{/* {news.map((nachricht) => <p>{showAllMessages(nachricht)}</p>)} */}
+					<>					
+						<p>{nachricht[searchWord]?nachricht[searchWord]: ''}</p>
+						{nachricht["title"]&&nachricht["created_at"]?
+						<li><a href={nachricht["url"]}>{nachricht["title"]}</a> (<a href={nachricht["url"]}>{shortUrl(nachricht["url"])}</a>)
+						<br /><span className="small">{nachricht["points"]} points {nachricht["author"]} {calculateDateDiff(nachricht["created_at"])} | hide | {nachricht["num_comments"]}  Comments</span></li>:''}	
+					</>
+			))}			
 			</ol>
-    </div>
-  );
+    	</div>
+  	);
 }
